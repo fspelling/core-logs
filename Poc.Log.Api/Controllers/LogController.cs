0a841f4a5bde33f854011230ae7d24bdcd92ee9e
Fernando Spelling <f.l.spelling@gmail.com>
2020-04-06 11:06:40 -0300

    criacao projeto

---
+++ Poc.Log.Api/Controllers/LogController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Poc.Log.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogController : Controller
+    {
+        private readonly ILogger<LogController> _logger;
+
+        public LogController(ILogger<LogController> logger)
+        {
+            _logger = logger;
+            _logger.LogDebug("NLog injetado no LogController");
+            _logger.Log()
+        }
+
+        public void Post()
+        {
+            _logger.LogInformation("test log");
+        }
+    }
+}
--- Poc.Log.Api/Program.cs
+++ Poc.Log.Api/Program.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
+using System;
 
 namespace Poc.Log.Api
 {
@@ -13,14 +10,35 @@ namespace Poc.Log.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+
+            try
+            {
+                logger.Debug("Serviço de log iniciado");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+                logger.Error(e, "Ocorreu algum erro no serviço de log");
+                throw;
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+                 .ConfigureWebHostDefaults(webBuilder =>
+                 {
+                     webBuilder.UseStartup<Startup>();
+                 })
+                .ConfigureLogging(logging =>
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                })
+                .UseNLog();
     }
 }
